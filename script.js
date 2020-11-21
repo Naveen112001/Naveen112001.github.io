ba24c2522f7fccaf05bfb1aded484afe364f15da
naveen2001k <54986966+naveen2001k@users.noreply.github.com>
2020-11-21 11:04:36 +0530

    uploading website

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-var myapp={}
-(function()
-{
-	myapp.add=function(a,b){
-		return a+b;
-	}
-})();
+const navSlide =() =>{
+    const bugger=document.querySelector('.bugger');
+    const nav = document.querySelector('.nav-links');
+    const navlinks=document.querySelectorAll('.nav-links li');
+    bugger.addEventListener('click',()=>{
+            nav.classList.toggle('nav-active');
+            navlinks.forEach((link,index)=>{
+                if(link.style.animation){
+                    link.style.animation='';
+                }
+                else
+                {
+                link.getElementsByClassName.animation=`navLinkFade 0.5s ease forwards ${index / 7 + 3}s`;
+                }
+            });
+            //Bugger animation
+            bugger.classList.toggle('toggle');
+    });
+   
+}
+navSlide();
